a4fc4b06fa57f4e36c0320d378cec0a4fb9f84bd
Nathanaël Lécaudé <natcl@users.noreply.github.com>
2016-03-09 16:54:20 -0500

    Added files

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ var settings = {
     httpAdminRoot:"/admin",
     httpNodeRoot: "/",
     userDir: __dirname,
+    flowFile: "flows.json",
     functionGlobalContext: { }    // enables global context
 };
 
